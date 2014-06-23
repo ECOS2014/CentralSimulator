fbfe6ef4a12f46835be0762675ddd801edabe58e
Paula Castellanos <pcastellanos>
2014-06-23 18:55:33 -0500

    implementación Leader followers, consulta a cache de la información de una notificación

--- src/uniandes/centralsimulator/alarms/processor/ThreadAlarmResolver.java
+++ src/uniandes/centralsimulator/alarms/processor/ThreadAlarmResolver.java
@@ -6,6 +6,7 @@ import java.util.Date;
 
 import uniandes.centralsimulator.alarms.actions.FactoryActions;
 import uniandes.centralsimulator.alarms.actions.IAction;
+import uniandes.centralsimulator.alarms.cache.CentralAlarmEvaluator;
 import uniandes.centralsimulator.alarms.entities.AlarmReceive;
 import uniandes.centralsimulator.alarms.entities.TypeNotification;
 
@@ -24,9 +25,9 @@ public class ThreadAlarmResolver implements Runnable{
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
 		Date currentDate = new Date();
 		String date= df.format(currentDate);
-		System.out.println(date);
+		System.out.println("Hora: "+date+" resuelta propiedad: "+this.alarm.getIdProperty() +" sensor: "+this.alarm.getIdSensor());
 		if(this.alarm.getTypeNotification().equals(TypeNotification.Alarm)){
-			action = FactoryActions.getInstance().getAcction(this.alarm.getIdProperty()+"_"+this.alarm.getIdSensor());
+			action = FactoryActions.getInstance().getAcction(CentralAlarmEvaluator.getInstance().getActionToDo(this.alarm.getIdProperty()+"_"+this.alarm.getIdSensor()));
 			action.execute();
 		}
 		AdminThreads.getInstance().putFollower(this); 
